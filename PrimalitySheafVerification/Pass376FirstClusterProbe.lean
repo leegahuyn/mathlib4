dc9072b96f1b9ea8a0de5c638d557f0496490b87
leegahuyn <102484661+leegahuyn@users.noreply.github.com>
2026-08-09 01:14:39 +0900

    test: inspect PASS 376 derivative kernel signatures

--- PrimalitySheafVerification/Pass376FirstClusterProbe.lean
+++ PrimalitySheafVerification/Pass376FirstClusterProbe.lean
@@ -1,54 +1,14 @@
 import Mathlib
 
-open Set
-
-noncomputable section
-
-section CanonicalCalculusInstances
-
-local attribute [-instance] Real.instAddCommGroup Complex.addCommGroup Semiring.toModule
-
-noncomputable def probeLeftParam (t : ℝ) : ℂ :=
-  (((-(1 / 2 : ℝ)) : ℂ) +
-    ((Real.sqrt 3 / 2 + t : ℝ) : ℂ) * Complex.I)
-
-noncomputable def probeRightParam (t : ℝ) : ℂ :=
-  ((((1 / 2 : ℝ)) : ℂ) +
-    ((Real.sqrt 3 / 2 + t : ℝ) : ℂ) * Complex.I)
-
-set_option backward.isDefEq.respectTransparency false in
-example (t : Set.Ici (0 : ℝ)) :
-    HasDerivAt probeLeftParam Complex.I (t : ℝ) := by
-  have hxR : HasDerivAt (fun _s : ℝ => -(1 / 2 : ℝ)) 0 t :=
-    hasDerivAt_const (t : ℝ) _
-  have hyR : HasDerivAt
-      (fun s : ℝ => Real.sqrt 3 / 2 + s) 1 t := by
-    simpa only [Pi.add_apply, id_eq, zero_add] using
-      (hasDerivAt_const (t : ℝ) (Real.sqrt 3 / 2)).add
-        (hasDerivAt_id (t : ℝ))
-  unfold probeLeftParam
-  simpa only [zero_add, one_mul] using
-    hxR.ofReal_comp.add (hyR.ofReal_comp.mul_const Complex.I)
-
-set_option backward.isDefEq.respectTransparency false in
-example (t : Set.Ici (0 : ℝ)) :
-    HasDerivAt probeRightParam Complex.I (t : ℝ) := by
-  have hxR : HasDerivAt (fun _s : ℝ => (1 / 2 : ℝ)) 0 t :=
-    hasDerivAt_const (t : ℝ) _
-  have hyR : HasDerivAt
-      (fun s : ℝ => Real.sqrt 3 / 2 + s) 1 t := by
-    simpa only [Pi.add_apply, id_eq, zero_add] using
-      (hasDerivAt_const (t : ℝ) (Real.sqrt 3 / 2)).add
-        (hasDerivAt_id (t : ℝ))
-  unfold probeRightParam
-  simpa only [zero_add, one_mul] using
-    hxR.ofReal_comp.add (hyR.ofReal_comp.mul_const Complex.I)
-
-example (t : ℝ) :
-    (((1 / 2 : ℝ) : ℂ) +
-        (((-t / (4 * Real.sqrt (1 - t ^ 2 / 4)) : ℝ) : ℂ) * Complex.I)) =
-      ({ re := 1 / 2
-         im := -t / (4 * Real.sqrt (1 - t ^ 2 / 4)) } : ℂ) := by
-  apply Complex.ext <;> simp
-
-end CanonicalCalculusInstances
+#print HasDerivAt
+#print HasFDerivAt
+#check @HasDerivAt
+#check @HasFDerivAt
+#check @hasDerivAt_const
+#check @hasFDerivAt_const
+#check @HasDerivAt.add
+#check @HasDerivAt.ofReal_comp
+#check @HasDerivAt.congr_of_eventuallyEq
+#check @HasFDerivAt.congr_of_eventuallyEq
+#check @hasDerivAt_iff_hasFDerivAt
+#check @hasFDerivAt_iff_isLittleO
